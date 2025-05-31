61b7bb26c1cba90f03bf72243dac7b1e9dfcfaee
Tapas Jyoti <tapasjyoti8327@gmail.com>
2025-06-01 00:47:00 +0530

    setup database

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// api endpoint
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => console.log("Server is Started at: ", port));
+  })
+  .catch((err) => {
+    console.error("Error connecting to database: ", err.message);
+  });
