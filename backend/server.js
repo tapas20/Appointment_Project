e71b6cacdf6df06dd974ed9b25ce4e36f6e134f3
Tapas Jyoti <tapasjyoti8327@gmail.com>
2025-06-05 01:45:11 +0530

    created doctor model

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
+connectCloudinary();
 
 // middleware
 app.use(express.json());
